6d38c4244aec9674274b33bd1399803667a52948
fatehero <65950846+fatehero@users.noreply.github.com>
2021-06-12 14:45:54 +0200

    fixed some bugs

--- godot_game/scripts/glo.gd
+++ godot_game/scripts/glo.gd
@@ -4,5 +4,5 @@ var player : KinematicBody2D
 var my_ships=[]
 
 func _ready():
-	for i in range(3):
-		my_ships.append(load("res://ship_types_resource/ship_type%"%i+".tres").duplicate())
+	for i in range(2):
+		my_ships.append(load("res://ship_types_resource/ship_type"+str(i)+".tres").duplicate())
