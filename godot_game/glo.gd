22319a1fe5ec261109e0007f5401af5a6ba3ea3b
fatehero <65950846+fatehero@users.noreply.github.com>
2021-06-12 14:44:23 +0200

    fix some bugs

---
+++ godot_game/glo.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var player:KinematicBody2D
+var my_ships=[]
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in range(2):
+		my_ships.append(load("res://ship_types_resource/ship_type%"%i+".tres").duplicate())
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
