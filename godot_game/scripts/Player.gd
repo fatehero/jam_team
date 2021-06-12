771069d56edb7c39a6d489d015618f8107e37fa8
litenhamster <71274875+litenhamster@users.noreply.github.com>
2021-06-12 12:48:38 +0200

    Added movement
    Move with W or S

--- godot_game/scripts/Player.gd
+++ godot_game/scripts/Player.gd
@@ -2,13 +2,19 @@ extends KinematicBody2D
 
 export var bullet_scene : PackedScene
 
+onready var forward = $Forward
+
 var motion : Vector2
-var speed = 100
+var speed = 300
 var rotation_speed = 250
 
 func _physics_process(delta):
-	motion *= speed
-	motion = move_and_slide(motion)
+	motion = global_position.direction_to(forward.global_position)
+	motion = motion.normalized() * speed
+	if Input.is_action_pressed("up"):
+		motion = move_and_slide(motion)
+	if Input.is_action_pressed("down"):
+		motion = move_and_slide(-motion)
 
 func _process(delta):
 	if Input.is_action_pressed("right"):
