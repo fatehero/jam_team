faba4112b49e34a698ca478850c47866c1f9198f
fatehero <65950846+fatehero@users.noreply.github.com>
2021-06-12 20:42:09 +0200

    fixed more

--- godot_game/scripts/Player.gd
+++ godot_game/scripts/Player.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 export var bullet_scene : PackedScene
 
 onready var forward = $Forward
-onready var anim_left = $left_half/AnimatedSprite
-onready var anim_right =$right_half/AnimatedSprite 
+onready var anim_left = $left_half/img
+onready var anim_right =$right_half/img
 
 var motion : Vector2
 var speed = 300
@@ -30,24 +30,32 @@ func _physics_process(delta):
 		motion = global_position.direction_to(forward.global_position)
 		motion = motion.normalized()
 		if up and down:
-			anim_left.animation = "idle"
-			anim_right.animation = "idle"
+			if anim_left!=null:
+				anim_left.animation = "idle"
+			if anim_right!=null:
+				anim_right.animation = "idle"
 			motion = Vector2.ZERO
 		elif up:
 			motion *= speed
 		else:
 			motion *= -speed
-		anim_left.animation = "move"
-		anim_right.animation = "move"
+		if anim_left!=null:	
+			anim_left.animation = "move"
+		if anim_right!=null:
+			anim_right.animation = "move"
 	else:
-		anim_left.animation = "idle"
-		anim_right.animation = "idle"
+		if anim_left!=null:
+		 anim_left.animation = "idle"
+		if anim_right!=null:
+			anim_right.animation = "idle"
 		motion = Vector2.ZERO
 	if Input.is_action_pressed("right"):
-		anim_left.animation = "move"
+		if anim_left!=null: 
+		 anim_left.animation = "move"
 		rotation_degrees += rotation_speed * delta
 	if Input.is_action_pressed("left"):
-		anim_right.animation = "move"
+		if anim_right!=null:
+		 anim_right.animation = "move"
 		rotation_degrees += -rotation_speed * delta
 
 	motion = move_and_slide(motion)
