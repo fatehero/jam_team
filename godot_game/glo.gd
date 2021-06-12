510dddc545e6b96ba3f48e18493a715f05c0ec50
fatehero <65950846+fatehero@users.noreply.github.com>
2021-06-12 14:09:32 +0200

    some change to global  script

--- godot_game/glo.gd
+++ godot_game/glo.gd
@@ -4,11 +4,14 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var player:KinematicBody2D
 
+var player:KinematicBody2D
+var my_ships=[]
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for i in range(3):
+		my_ships.append(load("res://ship_types_resource/ship_type%"%i+".tres").duplicate())
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- godot_game/scripts/enemy.gd
+++ godot_game/scripts/enemy.gd
@@ -4,8 +4,11 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+var close=200.0
+var too_close=80.0
+var distance=0.0
+var spd=50
+var dir=Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,3 +17,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _physics_process(delta):
+	distance=global_position.distance_to(glo.player.global_position)
+	if distance<close:
+		look_at(glo.player.global_position)
+		shot()
+		if distance>too_close:
+			dir=global_position.direction_to(glo.player.global_position)
+		
+		
+
+
+func shot():
+	pass	
