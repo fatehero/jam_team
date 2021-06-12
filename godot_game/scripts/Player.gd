e22e4e08208d7e6b9c2376b4dcd04fc3215b54a3
litenhamster <71274875+litenhamster@users.noreply.github.com>
2021-06-12 19:54:26 +0200

    Added animations
    They don't work properly but I'll push them for now

--- godot_game/scripts/Player.gd
+++ godot_game/scripts/Player.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 export var bullet_scene : PackedScene
 
 onready var forward = $Forward
+onready var anim_left = $left_half/AnimatedSprite
+onready var anim_right =$right_half/AnimatedSprite 
 
 var motion : Vector2
 var speed = 300
@@ -18,20 +20,34 @@ func set_halfs(val):
 
 func _ready():
 	glo.player = self
-	
-	
+	anim_left.playing = true
+	anim_right.playing = true
 
 func _physics_process(delta):
-	motion = global_position.direction_to(forward.global_position)
-	motion = motion.normalized() * speed
-	if Input.is_action_pressed("up"):
-		motion = move_and_slide(motion)
-	if Input.is_action_pressed("down"):
-		motion = move_and_slide(-motion)
-
-func _process(delta):
-	
+	var up = Input.is_action_pressed("up")
+	var down = Input.is_action_pressed("down")
+	if down or up:
+		motion = global_position.direction_to(forward.global_position)
+		motion = motion.normalized()
+		if up and down:
+			anim_left.animation = "idle"
+			anim_right.animation = "idle"
+			motion = Vector2.ZERO
+		elif up:
+			motion *= speed
+		else:
+			motion *= -speed
+		anim_left.animation = "move"
+		anim_right.animation = "move"
+	else:
+		anim_left.animation = "idle"
+		anim_right.animation = "idle"
+		motion = Vector2.ZERO
 	if Input.is_action_pressed("right"):
+		anim_left.animation = "move"
 		rotation_degrees += rotation_speed * delta
 	if Input.is_action_pressed("left"):
+		anim_right.animation = "move"
 		rotation_degrees += -rotation_speed * delta
+
+	motion = move_and_slide(motion)
